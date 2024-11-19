d63618ae0ee9f0a53316e7d1597d9c617eedcfd9
OSDVF <o.s.dv.f@seznam.cz>
2024-11-19 19:46:48 +0100

    Remove glfw from dependencies

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    var module = b.addModule("zig-glfw", .{
+    _ = b.addModule("zig-glfw", .{
         .target = target,
         .optimize = optimize,
         .root_source_file = b.path("src/main.zig"),
@@ -20,14 +20,4 @@ pub fn build(b: *std.Build) !void {
     });
     b.installArtifact(main_tests);
     test_step.dependOn(&b.addRunArtifact(main_tests).step);
-
-    if (b.lazyDependency("glfw", .{
-        .target = target,
-        .optimize = optimize,
-    })) |dep| {
-        module.linkLibrary(dep.artifact("glfw"));
-        @import("glfw").addPaths(module);
-        main_tests.linkLibrary(dep.artifact("glfw"));
-        @import("glfw").addPaths(&main_tests.root_module);
-    }
 }
